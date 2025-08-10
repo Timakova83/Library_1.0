3ebfc6d46b9861a0b2fe58804be0f1e165828239
Timakova83 <vv.timakova@gmail.com>
2025-08-10 17:22:15 +0300

    добавлена главная страница

--- library_app/views.py
+++ library_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.models import User
 
-# Create your views here.
+class HomePage(View):
+    def get(self, request):
+        return render(request, 'library_app/home.html')
+    
+class AuthPage(View):
+    def get(self, request):
+        return render(request, 'library_app/auth.html')
--- library_project/urls.py
+++ library_project/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from library_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePage.as_view(), name='urlHomePage'),
+    path('auth/', AuthPage.as_view(), name='urlAuthPage'),
 ]
